948fa7aebf4e64d12ce26571a57105bd1999477e
amir movassaghi <movassaghi6@gmail.com>
2024-12-30 19:26:50 +0330

    Integrate MongoDB user validation in Kafka consumer for Twitter login

--- app/api/user.py
+++ app/api/user.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from schemas import User
 from aiokafka import AIOKafkaProducer, AIOKafkaConsumer
 #from settings import loop
+from database import mongo_db
 from utils import parse_kafka_message
 import json
+
 
 
 router = APIRouter()
@@ -37,7 +39,20 @@ async def create_consumer():
     await consumer.start()
     try:
         async for msg in consumer:
-            parse_kafka_message(msg)
+            # get username from consumer's message
+            user = parse_kafka_message(msg).username
+
+            if mongo_db.client is None:
+                raise Exception("MongoDB client is not initialized.")
+            
+            # user from database 
+            user_in_db = await mongo_db.get_user_by_username(user)
+
+            if not user_in_db:
+                raise HTTPException(status_code=404, detail="User not found")
+            
+            return user_in_db
+        
 
     finally:
         await consumer.stop()
