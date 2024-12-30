860fffb594ec53fb17cf0ab44bce51908250c725
amir movassaghi <movassaghi6@gmail.com>
2024-12-30 19:24:14 +0330

    Add MongoDB connection management with Beanie ODM and async methods

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from api import user
+from database import create_start_app_handler, create_stop_app_handler
 import asyncio
 
 
@@ -8,3 +9,6 @@ app = FastAPI()
 app.include_router(user.router)
 asyncio.create_task(user.create_consumer())
 
+# Register startup and shutdown events
+app.add_event_handler("startup", create_start_app_handler(app))
+app.add_event_handler("shutdown", create_stop_app_handler(app))
