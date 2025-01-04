18da421cd11fe0d63d55d9b0b4047ec7d1c7c161
amir movassaghi <movassaghi6@gmail.com>
2025-01-04 17:34:06 +0330

    Refactor: decouple kafka producer function from route function
    Create a seperate script for kafka producer and consumer functions and configurations named kafka.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
-from api import user
-from database import create_start_app_handler, create_stop_app_handler
+from app.api import user
+from app.database import create_start_app_handler, create_stop_app_handler
+from app.kafka import create_consumer
 import asyncio
+
 
 
 app = FastAPI()
 
 app.include_router(user.router)
-asyncio.create_task(user.create_consumer())
+asyncio.create_task(create_consumer())
 
 # Register startup and shutdown events
 app.add_event_handler("startup", create_start_app_handler(app))
