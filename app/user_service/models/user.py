aefc0bacfa47f3e0338e6e6360451b21b0045609
amir movassaghi <movassaghi6@gmail.com>
2025-01-14 17:50:25 +0330

    Enhance User model validation and caching

--- app/user_service/models/user.py
+++ app/user_service/models/user.py
@@ -1,17 +1,22 @@
 from pydantic import Field
 from typing import Optional
 from beanie import Document, Indexed
+import datetime
 
 
 
 class User(Document):
     username: Indexed(str, unique=True)
-    hashed_password: str
-    phone_number: str = Field(unique=True)
+    hashed_password: str = Field(min_length=8)
+    phone_number: str = Field(unique=True, regex=r'^\+?98?\d{9,15}$')
     email: Optional[str] = None
 
     class Settings:
         collection = "users"
+        use_cache = True
+        cache_expiration_time = datetime.timedelta(seconds=100)
+        cache_capacity = 5
+
 
 
 class Task(Document):
